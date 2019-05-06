16caf796a7cc632bd578056b8b4a6c2d25c46105
rfc <reuben.fletchercostin@gmail.com>
2019-05-07 06:34:57 +1000

    msys2 boot script -- needs more ampersand

--- boot.ps1
+++ boot.ps1
@@ -6,17 +6,17 @@ $env:MSYS64_BASH = "$env:MSYS64_USR_BIN_DIR" + "\bash.exe"
 
 (get-command 'bash.exe').Path
 
-"$env:MSYS64_BASH" -l -c 'logout'
+& "$env:MSYS64_BASH" -l -c 'logout'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
-"$env:MSYS64_BASH" -l -c 'pacman -Syu --noconfirm mingw-w64-x86_64-python3-pip && logout'
+& "$env:MSYS64_BASH" -l -c 'pacman -Syu --noconfirm mingw-w64-x86_64-python3-pip && logout'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
 
-"$env:MSYS64_BASH" -c 'which python || true'
-"$env:MSYS64_BASH" -c 'python --version || true'
+& "$env:MSYS64_BASH" -c 'which python || true'
+& "$env:MSYS64_BASH" -c 'python --version || true'
 
-"$env:MSYS64_BASH" -c 'python -m pip -- install -r requirements.txt'
+& "$env:MSYS64_BASH" -c 'python -m pip -- install -r requirements.txt'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
-"$env:MSYS64_BASH" -c 'python -m pip -- install -e .'
+& "$env:MSYS64_BASH" -c 'python -m pip -- install -e .'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
-"$env:MSYS64_BASH" -c 'python -m pytest -- -k empty-rule .'
+& "$env:MSYS64_BASH" -c 'python -m pytest -- -k empty-rule .'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
