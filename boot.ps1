05066e7698d58da4923e9b7bbba4e188cad6d020
rfc <reuben.fletchercostin@gmail.com>
2019-05-07 06:28:10 +1000

    powershell msys boot script -- chuck errors if something fails

--- boot.ps1
+++ boot.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 $env:MSYS64_USR_BIN_DIR = "C:\tools\msys64\usr\bin\bash"
 $env:PATH = "$env:MSYS64_USR_BIN_DIR" + ";" + "$env:PATH"
 $env:MSYS64_BASH = "$env:MSYS64_USR_BIN_DIR" + "\bash.exe"
@@ -5,11 +7,16 @@ $env:MSYS64_BASH = "$env:MSYS64_USR_BIN_DIR" + "\bash.exe"
 (get-command 'bash.exe').Path
 
 "$env:MSYS64_BASH" -l -c 'logout'
+if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
 "$env:MSYS64_BASH" -l -c 'pacman -Syu --noconfirm mingw-w64-x86_64-python3-pip && logout'
+if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
 
 "$env:MSYS64_BASH" -c 'which python || true'
 "$env:MSYS64_BASH" -c 'python --version || true'
 
 "$env:MSYS64_BASH" -c 'python -m pip -- install -r requirements.txt'
+if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
 "$env:MSYS64_BASH" -c 'python -m pip -- install -e .'
+if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
 "$env:MSYS64_BASH" -c 'python -m pytest -- -k empty-rule .'
+if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
