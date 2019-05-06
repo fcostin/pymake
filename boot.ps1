579ac74f77791f0413e5aca75bc6b4b4d8e30042
rfc <reuben.fletchercostin@gmail.com>
2019-05-07 07:12:28 +1000

    msys2 boot working directory drama

--- boot.ps1
+++ boot.ps1
@@ -3,10 +3,11 @@ $ErrorActionPreference = "Stop"
 $env:MSYS64_USR_BIN_DIR = "C:\tools\msys64\usr\bin"
 $env:PATH = "$env:MSYS64_USR_BIN_DIR" + ";" + "$env:PATH"
 $env:MSYS64_BASH = "$env:MSYS64_USR_BIN_DIR" + "\bash.exe"
+$env:MSYS64_BOOT_SCRIPT = "$env:TRAVIS_BUILD_DIR" + "\boot.sh"
 
 (get-command 'bash.exe').Path
 
 & "$env:MSYS64_BASH" -l -c 'logout'
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
-& "$env:MSYS64_BASH" -c 'boot.sh'
+& "$env:MSYS64_BASH" -c "$env:MSYS64_BOOT_SCRIPT"
 if ($LASTEXITCODE -ne 0) { throw "nonzero exit code: $LASTEXITCODE" }
--- boot.sh
+++ boot.sh
@@ -1,11 +1,16 @@
 set -e -x
 
+cd "$TRAVIS_BUILD_DIR"
+
 pacman -Syu --noconfirm mingw-w64-x86_64-python3-pip
 
 which python
 python --version
 which pip
 pip --version
+
+pwd
+ls -l
 
 pip -- install -r requirements.txt
 pip -- install -e .
